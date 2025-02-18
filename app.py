ef073febfed8b6c8b7d8df2f7e383dca2892076b
Quan A Pham <92947725+quanaphamm@users.noreply.github.com>
2025-02-18 06:18:15 +0000

    add api

--- app.py
+++ app.py
@@ -23,6 +23,10 @@ def show_job(id):
     if not job:
         return "Not Found", 404
     return render_template('jobpage.html', job=job)
+@app.route("/api/job/<id>")
+def show_job_json(id):
+    job = load_job_from_db(id)
+    return jsonify(job)
     
 @app.route("/job/<id>/apply", methods = ['post'])
 def apply_to_job(id):
