54053b5cb760424193fdad60cec5bed074d3de3a
Quan A Pham <92947725+quanaphamm@users.noreply.github.com>
2025-02-18 03:02:31 +0000

    recheck

--- database.py
+++ database.py
@@ -5,10 +5,12 @@ db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(db_connection_string)
 
+
 def load_jobs_from_db():
-  with engine.connect() as conn:
-      result = conn.execute(text("select * from jobs"))
-      jobs = []
-      for row in result.mappings().all():
-          jobs.append(dict(row))
-  return jobs
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
+        jobs = []
+        for row in result.mappings().all():
+
+            jobs.append(dict(row))
+    return jobs
