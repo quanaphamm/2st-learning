d68d51d7063ab333b01b76dbe74e3a0a6f406d7b
Quan A Pham <92947725+quanaphamm@users.noreply.github.com>
2025-02-18 03:00:18 +0000

    new updated

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine, text
+import os
 
-db_connection_string = "mysql+pymysql://sql3763295:VbmzSwWIgr@sql3.freesqldatabase.com/sql3763295?charset=utf8mb4"
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
 engine = create_engine(db_connection_string)
-
-
 
 def load_jobs_from_db():
   with engine.connect() as conn:
